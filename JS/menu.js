b5a89cebb00d903573a010dcb52a6c1d70a96fda
Pedro Castro <vieiracastropedro7@gmail.com>
2024-05-12 13:23:37 -0300

    criei botões para alterar a cor do site

--- JS/menu.js
+++ JS/menu.js
@@ -1,4 +1,14 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector("#cabeçalho")
     header.classList.toggle("rolagem", this.window.scrollY > 390)
-}) 
+})
+
+function mudaCor(corSelecionada) {
+    if (corSelecionada == "roxo") {
+        document.documentElement.style.setProperty('--cor01', "#5a348f");
+    } else if (corSelecionada == "azul") {
+        document.documentElement.style.setProperty('--cor01', "#3a86ff");
+    } else {
+        document.documentElement.style.setProperty('--cor01', "#8ac926");
+    }
+}
