0a45b5e958fb14a5e391eb7142b0611644cf4f35
Pedro Castro <vieiracastropedro7@gmail.com>
2024-05-12 14:11:40 -0300

    Alterei os planos de fundo

--- JS/menu.js
+++ JS/menu.js
@@ -4,11 +4,16 @@ window.addEventListener("scroll", function(){
 })
 
 function mudaCor(corSelecionada) {
+    let banner = document.getElementById("banner");
+
     if (corSelecionada == "roxo") {
-        document.documentElement.style.setProperty('--cor01', "#5a348f");
+        document.documentElement.style.setProperty('--cor01', "#351143");
+        banner.style.backgroundImage = "url(imagens/background-roxo.jpg)";
     } else if (corSelecionada == "azul") {
-        document.documentElement.style.setProperty('--cor01', "#3a86ff");
+        document.documentElement.style.setProperty('--cor01', "#002A6D");
+        banner.style.backgroundImage = "url(imagens/background-azul.jpg)";
     } else {
-        document.documentElement.style.setProperty('--cor01', "#8ac926");
+        document.documentElement.style.setProperty('--cor01', "#006D66");
+        banner.style.backgroundImage = "url(imagens/background-verde.jpg)";
     }
 }
