d2797ec3fb0140fb62268683da264feabadf3e28
Pedro Castro <vieiracastropedro7@gmail.com>
2024-06-08 14:58:57 -0300

    carrossel e rolagem
    adicionei uma rolagem para o topo quando os botões de mudar o tema forem clicados, e implementei o carrossel de textos no topo da página

--- JS/menu.js
+++ JS/menu.js
@@ -1,10 +1,18 @@
+// Código para o header
 window.addEventListener("scroll", function(){
     let header = document.querySelector("#cabeçalho")
     header.classList.toggle("rolagem", this.window.scrollY > 390)
 })
 
+// Fução para mudar a cor tema
 function mudaCor(corSelecionada) {
     let banner = document.getElementById("banner");
+
+    // Faz com que a tela seja rolada para o topo
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 
     if (corSelecionada == "roxo") {
         document.documentElement.style.setProperty('--cor01', "#351143");
@@ -17,3 +25,16 @@ function mudaCor(corSelecionada) {
         banner.style.backgroundImage = "url(imagens/background-verde.jpg)";
     }
 }
+
+// código para o carrosel de textos
+let indexatual = 0;
+const items = document.querySelectorAll('.carousel-item');
+const totalItems = items.length;
+
+function showNextItem() {
+    items[indexatual].classList.remove('active');
+    indexatual = (indexatual + 1) % totalItems;
+    items[indexatual].classList.add('active');
+}
+
+setInterval(showNextItem, 5000);
