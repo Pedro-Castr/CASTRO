4e0c3d6e16364784357f8ffc1fc4a0493a923c72
Pedro Castro <vieiracastropedro7@gmail.com>
2024-06-15 14:13:02 -0300

    Alteração nos Planos de fundo

--- JS/menu.js
+++ JS/menu.js
@@ -16,13 +16,13 @@ function mudaCor(corSelecionada) {
 
     if (corSelecionada == "roxo") {
         document.documentElement.style.setProperty('--cor01', "#351143");
-        banner.style.backgroundImage = "url(imagens/background-roxo.jpg)";
+        banner.style.backgroundImage = "linear-gradient(to right bottom, #3e1646, #4a174d, #561854, #63175a, #701660, #7f1566, #8e136b, #9d106f, #b10d74, #c50877, #da0479, #ee037a)";
     } else if (corSelecionada == "azul") {
         document.documentElement.style.setProperty('--cor01', "#002A6D");
-        banner.style.backgroundImage = "url(imagens/background-azul.jpg)";
+        banner.style.backgroundImage = "linear-gradient(to right bottom, #071e49, #112d63, #1a3d7f, #224e9c, #2a60ba, #226dc7, #1679d3, #0086df, #008ed7, #0094cc, #0099bf, #2e9cb2)";
     } else {
         document.documentElement.style.setProperty('--cor01', "#006D66");
-        banner.style.backgroundImage = "url(imagens/background-verde.jpg)";
+        banner.style.backgroundImage = "linear-gradient(to right bottom, #004562, #00516a, #005e6f, #006970, #00756f, #007f6f, #00896c, #009266, #009d64, #00a761, #00b25c, #00bc55)";
     }
 }
 
